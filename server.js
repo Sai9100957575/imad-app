32a561bbddebefa31e17d369d482f46d78ec0e0d
Sai Anish <anish.garapati@gmail.com>
2017-08-11 19:03:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function(req, res){
-  res.send(createTemplate(data));
+  res.send(createTemplate(articleone));
 });
 app.get('/article-two', function(req, res){
    res.send('This is article-two.');
