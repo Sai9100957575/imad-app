6ab4c96e4e092a8be2678272a21232e73928670d
Sai Anish <anish.garapati@gmail.com>
2017-08-11 19:02:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ var articleone = {
 };
 
 function createTemplate(data){
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
     var htmlTemplate = `<html>
 <head>
     <title>
@@ -38,7 +42,7 @@ function createTemplate(data){
         ${heading}
     </div>
     <div>
-    ${content}
+    ${date}
     </div>
 </body>
 
@@ -50,7 +54,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function(req, res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(data));
 });
 app.get('/article-two', function(req, res){
    res.send('This is article-two.');
